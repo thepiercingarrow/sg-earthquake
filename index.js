b74629e107e0d83d82bdbf21efc26dbc18124028
Mark Wright <thepiercingarrow@gmail.com>
2016-04-25 16:22:53 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('new room message', function(msg){
+    io.emit('new room message', msg);
+  });
+  socket.on('new whisper', function(msg){
+    io.emit('new whisper', msg);
+  });
+});
+
+http.listen(process.env.PORT, function(){
+  console.log('listening on port ' + process.env.PORT);
+});
