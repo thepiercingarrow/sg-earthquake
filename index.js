ac83f53c4579b21c54039c980e58b9fabf24cbf7
Mark Wright <thepiercingarrow@gmail.com>
2016-04-26 19:01:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT, function(){
+  console.log('listening on port ' + process.env.PORT);
 });
