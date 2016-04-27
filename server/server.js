e33576a036b5bd252e1600d518784f4725ae71a0
Mark Wright <thepiercingarrow@gmail.com>
2016-04-27 00:08:43 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '../client'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
