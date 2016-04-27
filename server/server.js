1378b1a26d1751e9ec1abd13f70dadd4329c88cc
Mark Wright <thepiercingarrow@gmail.com>
2016-04-27 03:17:58 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
+var fs = require('fs');
 var path = require('path');
 
 app.get('/', function (req, res) {
