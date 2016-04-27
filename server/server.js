f37ae7c00b02686f0b4f5f59eacc9251ccb5ed6b
Mark Wright <thepiercingarrow@gmail.com>
2016-04-27 01:56:31 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var path = require('path');
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/../client/index.html');
+  res.sendFile(path.normalize(__dirname + '/../client/index.html'));
 });
 // app.use(express.static('client'));
 
