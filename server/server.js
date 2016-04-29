4919585cd34566a0e0021d826ebf40ec2a43560e
Mark Wright <thepiercingarrow@gmail.com>
2016-04-28 21:02:28 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,5 +15,5 @@ io.on('connection', function(socket){
   });
 });
 
-app.listen(process.env.PORT || 8000);
+http.listen(process.env.PORT || 8000);
 console.log('listening on port ' + process.env.PORT);
