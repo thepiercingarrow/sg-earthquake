abc2ceb2756356f34a10bf240a7f1b7072e95698
Mark Wright <thepiercingarrow@gmail.com>
2016-05-08 18:20:53 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var path = require('path');
+
+var players = {};
 
 app.use(express.static('client'));
 
 io.on('connection', function(socket){
   socket.on('message', function(text){
     io.emit('message', {color: "blue", text: text});
+  });
+  socket.on('player-update', function(p){
+    players[p.name] = p;
+    io.emit('players', players);
   });
 });
 
