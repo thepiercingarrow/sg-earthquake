57f4e7a67c5320c2e1056f27bafce685efeb8643
Mark Wright <thepiercingarrow@gmail.com>
2016-05-09 15:30:41 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,13 +10,20 @@ var players = {};
 app.use(express.static('client'));
 
 io.on('connection', function(socket){
+    var name;
     socket.on('message', function(text){
 	io.emit('message', {color: "blue", text: text});
     });
     socket.on('player-update', function(p){
+	name = p.name;
 	players[p.name] = p;
 	io.emit('players', players);
+    	console.log("player updated: " + players[name].name);
     });
+    socket.on('disconnect, function(p){
+    	console.log("player disconnected: " + players[name].name);
+	delete players[name];
+    })
 });
 
 http.listen(process.env.PORT || 8000);
