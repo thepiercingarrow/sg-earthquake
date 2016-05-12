6f52b5b6cddfd6df46e4e4cc05ec89aaae9ebc06
Mark Wright <thepiercingarrow@gmail.com>
2016-05-12 19:08:51 -0400

    Update chat.js

--- client/chat.js
+++ client/chat.js
@@ -1,6 +1,8 @@
 var socket = io();
 var name = prompt("Enter a nickname.");
 
+var messages = document.getElementById('messages');
+
 $('.chatbar').bind("enterKey",function(e){
     socket.emit('message', name + ": " + $('.chatbar').val());
     var msg = $('.chatbar').val();
@@ -17,6 +19,15 @@ socket.on('message', function(msg){
     appendmessage(msg.text, msg.color);
 });
 
-function appendmessage(text, color) {
-    $('#messages').append($('<li style="color:' + color + '">').text(text));
+function appendmessage(msg, type, player) {
+    if (messages.childNodes.length > 10) {
+        messages.removeChild(chatList.childNodes[0]);
+    }
+    var newmsg = document.createElement('li');
+    if (type == "player") {
+        newmsg.innerHTML = "<i>" + player + "</i>" + msg;
+    }
+    newmsg.className = type;
+    newmsg.innerHTML = msg;
+    messages.appendChild(newmsg);
 }
