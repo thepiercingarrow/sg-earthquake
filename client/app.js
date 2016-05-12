e713d1e10690ff3d06d8fe2c7cc047a62b356f8c
Mark Wright <thepiercingarrow@gmail.com>
2016-05-12 18:02:21 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -21,9 +21,11 @@ players[name] = {name: name,
             size: 1/20
           };
 var ct = c.getContext("2d");
+
 ct.drawCircle = function(x,y,r){
     this.moveTo(x+r,y);
     this.arc(x,y,r,0,TAU);
+    appendmessage("drew circle | x: " + x + ", y:" + y + ", r: " + r);
 };
 
 function update(e) {
