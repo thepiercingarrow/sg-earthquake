9910b4804137522eaf714616b4c935e0bba37784
Mark Wright <thepiercingarrow@gmail.com>
2016-05-11 23:00:33 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,22 +1,3 @@
-var socket = io();
-var name = prompt("Enter a nickname.");
-
-$('.chatbar').bind("enterKey",function(e){
-    socket.emit('message', name + ": " + $('.chatbar').val());
-    var msg = $('.chatbar').val();
-    $('.chatbar').val('');
-    return false;
-});
-
-$('.chatbar').keyup(function(e){
-    if(e.keyCode == 13)
-	$(this).trigger("enterKey");
-});
-
-socket.on('message', function(msg){
-    $('#messages').append($('<li style="color:' + msg.color + '">').text(msg.text));
-});
-
 socket.on('players', function(p){
     players = p;
 });
