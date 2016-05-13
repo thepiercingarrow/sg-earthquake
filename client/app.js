204ea534e9ba85b1b1811ceed7385b214be68c23
Mark Wright <thepiercingarrow@gmail.com>
2016-05-12 21:22:52 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -2,25 +2,12 @@ socket.on('players', function(p){
     players = p;
 });
 
-var W = window.innerWidth, H = window.innerHeight;
-
-var c = document.getElementById("canvas");
-
-c.width = W; c.height = H;
-
 var players = {};
 players[name] = {name: name,
             x: W/2,
             y: H/2,
             size: 1/20
           };
-var ct = c.getContext("2d");
-
-ct.drawCircle = function(x,y,r){
-    this.moveTo(x+r,y);
-    this.arc(x,y,r,0,TAU);
-    appendmessage("drew circle | x: " + x + ", y: " + y + ", r: " + r, "dbg");
-};
 
 function update(e) {
     socket.emit('player-update', {name: name, x: e.clientX, y: e.clientY});
