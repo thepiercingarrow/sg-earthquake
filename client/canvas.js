ac29b60805e673032b6209194e8f36925b7615d3
Mark Wright <thepiercingarrow@gmail.com>
2016-05-12 21:31:58 -0400

    rm dbg

--- client/canvas.js
+++ client/canvas.js
@@ -9,5 +9,4 @@ var ct = c.getContext("2d");
 ct.drawCircle = function(x,y,r){
     this.moveTo(x+r,y);
     this.arc(x,y,r,0,TAU);
-    appendmessage("drew circle | x: " + x + ", y: " + y + ", r: " + r, "dbg");
 };
