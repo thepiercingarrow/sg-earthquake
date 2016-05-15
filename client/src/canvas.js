af7a097ec61546756b6ca5bc751cb8ef11d09e86
Mark Wright <thepiercingarrow@gmail.com>
2016-05-14 21:48:29 -0400

    Update canvas.js

--- client/src/canvas.js
+++ client/src/canvas.js
@@ -8,12 +8,12 @@ var g = c.getContext("2d");
 
 g.font = "20px Monaco";
 
-g.drawCircle = fungion(x,y,r){
+g.drawCircle = function(x,y,r){
     this.moveTo(x+r,y);
     this.arc(x,y,r,0,TAU);
 };
 
-window.addEventListener('resize', fungion(){
+window.addEventListener('resize', function(){
     W = window.innerWidth, c.width = W;
     H = window.innerHeight, c.height = H;
 });
