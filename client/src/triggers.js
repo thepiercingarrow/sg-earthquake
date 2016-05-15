726a44c28d7789139c59655c23b6b836c34a623b
Mark Wright <thepiercingarrow@gmail.com>
2016-05-15 01:27:43 -0400

    Create triggers.js

---
+++ client/src/triggers.js
@@ -0,0 +1,21 @@
+var mouseX, mouseY;
+
+c.addEventListener('mousemove', function(e){
+    mouseX = e.clientX, mouseY = e.clientY;
+});
+
+c.addEventListener('mousedown', function(e){
+    mouseDown = true;
+});
+
+c.addEventListener('mousedown', function(e){
+    mouseDown = false;
+});
+
+c.addEventListener('keydown', function(e){
+    //TODO
+});
+
+c.addEventListener('keyup', function(e){
+    //TODO
+});
