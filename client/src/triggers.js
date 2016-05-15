6dcf6cbe596da3bd8f605d1170276caf5478f256
Mark Wright <thepiercingarrow@gmail.com>
2016-05-15 19:28:07 -0400

    triggers

--- client/src/triggers.js
+++ client/src/triggers.js
@@ -1,4 +1,8 @@
 var mouseX, mouseY, mouseDown;
+var KEY_R = 82,
+    KEY_T = 84,
+    KEY_SPACE = 32,
+    KEY_ENTER = 13;
 
 c.addEventListener('mousemove', function(e){
     mouseX = e.clientX, mouseY = e.clientY;
@@ -12,18 +16,29 @@ c.addEventListener('mousedown', function(e){
     mouseDown = false;
 });
 
+function canvas_input(key, state) {
+    switch (key) {
+        case KEY_T:
+	    chatbar.focus(); break;
+        case KEY_R:
+	    input.grapple = state; break;
+        case KEY_SPACE:
+	    input.shield = state; break;
+    }
+}
+
 c.addEventListener('keydown', function(e){
-    //TODO
+    canvas_input(e.keyCode, true);
 });
 
 c.addEventListener('keyup', function(e){
-    //TODO
+    canvas_input(e.keyCode, false);
 });
 
-$('.chatbar').keyup(function(e){
-    if(e.keyCode == 13) {
-	socket.emit('p_message', chatbar.value);
+chatbar.addEventListener('keydown', function(e){
+    if (e.keyCode == KEY_ENTER) {
+        socket.emit('p_message', chatbar.value);
         chatbar.value = "";
-        return false;
+	c.focus();
     }
 });
