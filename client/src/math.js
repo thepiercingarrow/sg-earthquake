74ea502a5109b3eff5397539fc42cd29ed57bd73
Mark Wright <thepiercingarrow@gmail.com>
2016-05-15 23:30:20 -0400

    Update math.js

--- client/src/math.js
+++ client/src/math.js
@@ -5,6 +5,18 @@ function dist(x1, y1, x2, y2){
     return Math.sqrt(dx*dx + dy*dy);
 }
 
-function objcp(o1, 
+function objcp(o1, o2) {
+    for (var p in o1)
+        o2[p] = o1[p];
+}
 
-TODO
+function objcmp(o1, o2) {
+    var eq = true;
+    for (var p in o1) {
+        if (o1[p] != o2[p]) {
+            o1[p] = o2[p];
+            eq = false;
+        }
+    }
+    return eq;
+}
