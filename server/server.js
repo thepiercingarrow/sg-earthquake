97ac9525506e5257c3788347aa10084c79b2f52b
Mark Wright <thepiercingarrow@gmail.com>
2016-05-16 01:51:07 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,42 @@
 var express = require('express'), app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 var path = require('path');
 
 var port = process.env.PORT;
-
-var players = {};
-
-app.use(express.static('client'));
-
-io.on('connection', function(socket){
-    socket.username = 'Unnamed grappler';
-    socket.on('chat', function(msg){
-	io.emit('message', {msg: msg, type: 'p', player: socket.name});
-    });
-    socket.on('player-update', function(p){
-	name = p.name;
-	players[p.name] = p;
-	io.emit('players', players);
-    });
-    socket.on('disconnect', function(p){
-	io.emit('message', {msg: socket.username + ' has disconnected', type: "sys"});
-    })
-});
-
 if (!port) {
     console.log('Please specify a port.');
     process.exit(1);
 }
 http.listen(port);
 console.log('listening on port %d', port);
+app.use(express.static('client'));
+
+
+var players = {};
+
+io.on('connection', function(socket){
+    socket.username = 'Unnamed grappler';
+
+    socket.on('player-update', function(p){
+	name = p.name;
+	players[p.name] = p;
+	io.emit('players', players);
+    });
+
+    socket.on('spawn', function(p){
+	if (p.username != socket.username) {
+            io.emit('message', {msg: socket.username + ' has changed their name to ' + p.username + '.', type: 'alert'});
+            socket.username = p.username;
+        }
+        socket.join('arena');
+    });
+
+    socket.on('chat', function(msg){
+	io.emit('message', {msg: msg, type: 'p', player: socket.name});
+    });
+
+    socket.on('disconnect', function(p){
+	io.emit('message', {msg: socket.username + ' has disconnected', type: "sys"});
+    })
+});
