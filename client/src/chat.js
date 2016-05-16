e4350b8409590a652be76608db1af5f1b4a49f06
Mark Wright <thepiercingarrow@gmail.com>
2016-05-16 00:37:57 -0400

    moving socket to new file

--- client/src/chat.js
+++ client/src/chat.js
@@ -1,12 +1,7 @@
-var socket = io();
 var name = prompt("Enter a nickname.");
 
 var messages = document.getElementById('messages');
 var chatbar = document.getElementById('chatbar');
-
-socket.on('p_message', function(m){
-    appendmessage(m.msg, m.type, m.player);
-});
 
 function appendmessage(msg, type, player) {
     if (messages.childNodes.length > 10) {
