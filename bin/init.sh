689644ff4d1d337aca0a3f404f5a5f6bc178b6b0
Mark Wright <thepiercingarrow@gmail.com>
2016-05-19 21:23:57 -0400

    init file

---
+++ bin/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# run this script while at
+# the root of this distribution
+# to configure server settings
+
+# run `bin/init.sh`, then
+# answer the prompts
+
+# run as many times as you
+# want to change the server
+# settings
+
+# 
