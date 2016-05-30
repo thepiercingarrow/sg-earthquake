8b6d44938d846fa3f487c6725605a380eac6d2d6
Mark Wright <thepiercingarrow@gmail.com>
2016-05-30 17:07:04 -0400

    lol oops

--- client/src/app.js
+++ client/src/app.js
@@ -54,14 +54,6 @@ namebar.addEventListener('change', function(e){
     name = namebar.value
 });
 
-play.addEventListener('click', start);var menu = document.getElementById('menuwrapper');
-var namebar = document.getElementById('name'), name;
-var play = document.getElementById('play');
-
-namebar.addEventListener('change', function(e){
-    name = namebar.value
-});
-
 play.addEventListener('click', start);
 
 var messages = document.getElementById('messages');
