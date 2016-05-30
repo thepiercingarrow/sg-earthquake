648c7a6c9ca58eb6544c074526844ba64de7f3e9
Mark Wright <thepiercingarrow@gmail.com>
2016-05-30 15:54:49 -0400

    combine

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,89 @@
+var socket = io();
+
+socket.on('players', function(p){
+    players = p;
+});
+
+socket.on('message', function(m){
+    appendmessage(m.msg, m.type, m.player);
+});
+
+var TAU = 2 * Math.PI;
+
+function dist(x1, y1, x2, y2) {
+    var dx = x1 - x2, dy = y1 - y2;
+    return Math.sqrt(dx*dx + dy*dy);
+}
+
+function objcmp(o1, o2) {
+    var eq = true;
+    for (var p in o2) {
+        if (o1[p] != o2[p]) {
+            o1[p] = o2[p];
+            eq = false;
+        }
+    }
+    return eq;
+}
+
+var W = window.innerWidth, H = window.innerHeight;
+
+var canvas = document.getElementById("canvas");
+
+canvas.width = W; canvas.height = H;
+
+var g = canvas.getContext("2d");
+
+g.font = "20px Monaco";
+
+g.drawCircle = function(x,y,r){
+    this.moveTo(x+r,y);
+    this.arc(x,y,r,0,TAU);
+};
+
+window.addEventListener('resize', function(){
+    W = window.innerWidth, canvas.width = W;
+    H = window.innerHeight, canvas.height = H;
+});
+
+var menu = document.getElementById('menuwrapper');
+var namebar = document.getElementById('name'), name;
+var play = document.getElementById('play');
+
+namebar.addEventListener('change', function(e){
+    name = namebar.value
+});
+
+play.addEventListener('click', start);var menu = document.getElementById('menuwrapper');
+var namebar = document.getElementById('name'), name;
+var play = document.getElementById('play');
+
+namebar.addEventListener('change', function(e){
+    name = namebar.value
+});
+
+play.addEventListener('click', start);
+
+var messages = document.getElementById('messages');
+var chatbar = document.getElementById('chatbar');
+
+function appendmessage(msg, type, player) {
+    if (messages.childNodes.length > 10) {
+        messages.removeChild(messages.childNodes[0]);
+    }
+    var newmsg = document.createElement('li');
+    newmsg.className = type;
+    if (type == 'p')
+        newmsg.innerHTML = "<b>" + player + ": </b>" + msg;
+    else
+        newmsg.innerHTML = msg;
+    messages.appendChild(newmsg);
+}
+
+function dbg(msg) {
+    appendmessage(msg, 'dbg');
+}
+
 var players = {};
 players[name] = {name: name,
             x: W/2,
