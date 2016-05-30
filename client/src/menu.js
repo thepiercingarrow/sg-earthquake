642206cca588091db0f683db404abbda54fad7a6
Mark Wright <thepiercingarrow@gmail.com>
2016-05-30 15:53:48 -0400

    Delete menu.js

--- client/src/menu.js
+++
@@ -1,9 +0,0 @@
-var menu = document.getElementById('menuwrapper');
-var namebar = document.getElementById('name'), name;
-var play = document.getElementById('play');
-
-namebar.addEventListener('change', function(e){
-    name = namebar.value
-});
-
-play.addEventListener('click', start);
