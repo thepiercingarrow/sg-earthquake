612a33f3b1d07aa15782293f47e64dc82b0a6ea5
Mark Wright <thepiercingarrow@gmail.com>
2016-06-04 14:18:23 -0400

    Create draw.js

---
+++ client/modules/draw.js
@@ -0,0 +1,11 @@
+exports.circle = function(g, x, y, r){
+    g.beginPath();
+    this.arc(x, y, r, 0, 2 * Math.PI);
+    g.stroke();
+    g.closePath();
+};
+
+exports.label = function(g, x, y, l){g.font = "20px Monaco";
+	g.textAlign = "center";
+	g.fillText(players[p].name, x, y - 35);
+}
