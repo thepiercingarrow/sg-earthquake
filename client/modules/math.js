e192e565f7682e20dfcbafcabc33790275a276be
Mark Wright <thepiercingarrow@gmail.com>
2016-06-04 17:33:26 -0400

    no more import :(

--- client/modules/math.js
+++ client/modules/math.js
@@ -1,11 +1,11 @@
-exports.TAU = 2 * Math.PI;
+math.TAU = 2 * Math.PI;
 
-exports.dist = function(x1, y1, x2, y2) {
+math.dist = function(x1, y1, x2, y2) {
     var dx = x1 - x2, dy = y1 - y2;
     return Math.sqrt(dx*dx + dy*dy);
 }
 
-exports.objcmp = function(o1, o2) {
+math.objcmp = function(o1, o2) {
   var eq = true;
   for (var p in o2) {
   if (o1[p] != o2[p]) {
