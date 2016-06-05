13a6b8180ee764eb2005733674ffd091874a7934
Mark Wright <thepiercingarrow@gmail.com>
2016-06-05 11:01:07 -0400

    KEYBOARD WORKS YAYAYAY

--- server/module.js
+++ server/module.js
@@ -11,39 +11,39 @@ http.listen(port);
 console.log('listening on port %d', port);
 app.use(express.static('client'));
 
-var players = {};
-var grapplers = {};
-var arena = {
+exports.players = {};
+exports.grapplers = {};
+exports.arena = {
     grapplers: {}
 };
-var tick = 0;
+exports.tick = 0;
 
 io.on('connection', onconnect);
 
 function onconnect(socket) {
-    players[socket.id] = {};
-    var player = players[socket.id];
+    exports.players[socket.id] = {};
+    exports.player = exports.players[socket.id];
     io.emit('msg', {type: 'sys', msg: 'An unnamed grappler has connected.'});
-    player.name = 'Unnamed grappler';
+    exports.player.name = 'Unnamed grappler';
 
     socket.on('spawn', () => {
-	grapplers[socket.id] = {name: player.name, input: {}};
-        arena.grapplers[socket.id] = {name: player.name, x: 50, y: 50};
+	exports.grapplers[socket.id] = {name: exports.player.name, input: {}};
+        exports.arena.grapplers[socket.id] = {name: exports.player.name, x: 50, y: 50};
 	socket.join('arena');
-	socket.emit('spawned', arena);
+	socket.emit('spawned', exports.arena);
     });
 
     socket.on('new-input', input => {
-    	if (grapplers[socket.id] != undefined)
-	    grapplers[socket.id].input = input;
+    	if (exports.grapplers[socket.id] != undefined)
+	    exports.grapplers[socket.id].input = input;
     });
 
     socket.on('name-change', name => {
-	io.emit('msg', {type: 'sys', msg: '\'' + player.name + '\' has changed their name to \'' + name + '\'.'});
-	player.name = name;
-	if (grapplers[socket.id]) {
-	    grapplers[socket.id].name = name;
-	    arena.grapplers[socket.id].name = name;
+	io.emit('msg', {type: 'sys', msg: '\'' + exports.player.name + '\' has changed their name to \'' + name + '\'.'});
+	exports.player.name = name;
+	if (exports.grapplers[socket.id]) {
+	    exports.grapplers[socket.id].name = name;
+	    exports.arena.grapplers[socket.id].name = name;
 	}
     });
 
@@ -53,28 +53,18 @@ function onconnect(socket) {
 
     socket.on('disconnect', () => {
 	io.emit('msg', {msg: '\'' + player.name + '\' has disconnected', type: "sys"});
-	delete grapplers[socket.id];
-	delete grapplers[socket.id];
+	delete exports.grapplers[socket.id];
+	delete exports.arena.grapplers[socket.id];
     });
 }
 
 function send_arena() {
-    io.to('arena').emit('arena-update', arena);
-    console.log(arena);
+    io.to('arena').emit('arena-update', exports.arena);
 }
 
-function START(){
+function START(physics){
     setInterval/*(function(){eval*/(physics/*)}*/, 15);
     setInterval(send_arena, 45);
 };
 
 exports.START = START;
-
-function physics(){
-    for (var key in grapplers) {
-	g = arena.grapplers[key];
-	g.x = grapplers[key].input.mouseX;
-	g.y = grapplers[key].input.mouseY;
-    };
-    ++tick;
-}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 // var physics = require('fs').readFile('./physics.js', "utf8");
 var sg = require('./module.js');
-sg.START();
+sg.START(physics);
 
+function physics(){
+    for (var key in sg.grapplers) {
+	sg.g = sg.arena.grapplers[key];
+	sg.g.x = sg.grapplers[key].input.mouseX;
+	sg.g.y = sg.grapplers[key].input.mouseY;
+    };
+    ++sg.tick;
+}
