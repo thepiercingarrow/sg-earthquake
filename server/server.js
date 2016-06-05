758a7a1c273e64733bc63117bc1b8ed7042c0ecd
Mark Wright <thepiercingarrow@gmail.com>
2016-06-05 00:56:24 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,29 +21,29 @@ var tick = 0;
 io.on('connection', onconnect);
 
 function onconnect(socket) {
-    players.socket.id = {};
-    var player = players.socket.id;
+    players[socket.id] = {};
+    var player = players[socket.id];
     io.emit('msg', {type: 'sys', msg: 'An unnamed grappler has connected.'});
     player.name = 'Unnamed grappler';
 
     socket.on('spawn', () => {
-	grapplers.socket.id = {name: player.name, input: {}};
-        arena.grapplers.socket.id = {name: player.name, x: 50, y: 50};
+	grapplers[socket.id] = {name: player.name, input: {}};
+        arena.grapplers[socket.id] = {name: player.name, x: 50, y: 50};
 	socket.join('arena');
 	socket.emit('spawned');
     });
 
     socket.on('new-input', input => {
-    	if (grapplers.socket.id != undefined)
-	    grapplers.socket.id.input = input;
+    	if (grapplers[socket.id] != undefined)
+	    grapplers[socket.id].input = input;
     });
 
     socket.on('name-change', name => {
 	io.emit('msg', {type: 'sys', msg: '\'' + player.name + '\' has changed their name to \'' + name + '\'.'});
 	player.name = name;
-	if (grapplers.socket.id) {
-	    grapplers.socket.id.name = name;
-	    arena.grapplers.socket.id.name = name;
+	if (grapplers[socket.id]) {
+	    grapplers[socket.id].name = name;
+	    arena.grapplers[socket.id].name = name;
 	}
     });
 
@@ -53,8 +53,8 @@ function onconnect(socket) {
 
     socket.on('disconnect', () => {
 	io.emit('msg', {msg: '\'' + player.name + '\' has disconnected', type: "sys"});
-	delete grapplers.socket.id;
-	delete grapplers.socket.id;
+	delete grapplers[socket.id];
+	delete grapplers[socket.id];
     });
 }
 
